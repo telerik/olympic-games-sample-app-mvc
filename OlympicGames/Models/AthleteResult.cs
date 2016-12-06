006f04ae5d51a2b80cf68f558f8ba8395c741dca
Yanko Dzhemerenov <iankodj@gmail.com>
2016-12-06 19:15:01 +0200

    Add view for Top Results by Sport closes #9

---
+++ OlympicGames/Models/AthleteResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OlympicGames.Models
+{
+    public class AthleteResult
+    {
+        public decimal NumResult { get; set; }
+        public string Result { get; set; }
+        public string Name { get; set; }
+    }
+}
--- OlympicGames/WebAPIControllers/ResultsController.cs
+++ OlympicGames/WebAPIControllers/ResultsController.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using OlympicGames.Models;
+using System.Text.RegularExpressions;
 
 namespace OlympicGames.WebApiControllers
 {
@@ -32,8 +33,9 @@ namespace OlympicGames.WebApiControllers
             return db.results;
         }
 
-        //GET: api/Results/5
+        //GET: api/Result/5
         [ResponseType(typeof(result))]
+        [Route("api/result/GetMedalsByCountry")]
         public IHttpActionResult GetResult(int id)
         {
             result sport = db.results.Single(x => x.id == id);
@@ -154,6 +156,65 @@ namespace OlympicGames.WebApiControllers
             }
 
             return Ok(new { Data = data });
+        }
+
+        [Route("api/results/GetTopResults")]
+        [ResponseType(typeof(IList<AthleteResult>))]
+        public IHttpActionResult GetTopResults([FromUri] int sportId)
+        {
+            
+            var athletes = db.athletes.ToDictionary(key => key.id, value => String.Format("{0} {1}", value.firstName, value.lastName));
+
+            var results = db.results.Where(r => r.@event == sportId &&
+                                                r.result1 != string.Empty &&
+                                                r.result1 != "n/a")
+                                    .ToList()
+                                    .Select(item => new AthleteResult
+                                                    {
+                                                        Name = athletes[item.athlete],
+                                                        Result = isScoreResult(sportId) ? item.result1 + " points":
+                                                                 ((item.result1.Split('.')[0].Length == 1) ? "0:0" : "0:") + item.result1,
+                                                        NumResult = isScoreResult(sportId) ?
+                                                            Decimal.Parse(item.result1) :
+                                                            getTimeResult(item.result1)
+                                                    });
+
+            
+            if (isScoreResult(sportId))
+            {
+                results = results.OrderByDescending(ar => ar.NumResult).Take(10);
+            }
+            else
+            {
+                results = results.OrderBy(ar => ar.NumResult).Take(10);
+            }
+
+            return Ok(results);
+        }
+
+        private bool isScoreResult(int sportId)
+        {
+            int[] sportsWithScoreResult = new int[] { 154, 155, 896, 405, 437 };
+            return sportsWithScoreResult.Contains(sportId);
+        }
+
+        private decimal getTimeResult(string result)
+        {
+            string[] resultSections = result.Split('.');
+            string[] secondsSection = resultSections[0].Split(':');
+            double resultInSeconds = 0;
+            for (int i = 0; i < secondsSection.Count(); i++)
+            {
+                resultInSeconds += int.Parse(secondsSection[i]) * (int) Math.Pow(60, secondsSection.Count() - i - 1);
+            }
+
+            if (resultSections.Length > 1)
+            {
+                string miliseconds = resultSections[1].Split(' ')[0];
+                resultInSeconds += double.Parse(miliseconds) / (double) Math.Pow(10, miliseconds.Length);
+            }
+
+            return (decimal) resultInSeconds;
         }
     }
 }
